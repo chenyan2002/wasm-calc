b16f0b1d04e639c5380779cb142eb44f50a03da1
Yan Chen <yan.chen@fastly.com>
2025-06-10 15:10:21 +0900

    add wave

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -2,7 +2,6 @@ mod bindings;
 use bindings::docs::calculator::calculate::{self, Op};
 
 fn main() {
-  let res = calculate::eval_expression(Op::Add, 2, 3);
-  println!("{res}");
+    let res = calculate::eval_expression(Op::Add, 2, 3);
+    println!("{res}");
 }
-
